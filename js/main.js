71fc54e17a6c67b4bdf266c2973183d8570cc6db
Mohammadpro <crestin38@hotmail.com>
2023-11-20 19:16:16 +0330

    rebuild and modified project

--- js/main.js
+++ js/main.js
@@ -9,21 +9,30 @@ let voices = [];
 
 //seech.text = content_textbox.value;
 listen_btn.addEventListener('click',speak);
-
 let voiceselect = document.querySelector('.voiceselect');
-//voiceselect.setAttribute('class','voiceselect');
-//voiceselect.setAttribute('data-name','enable');
-//dropdown.append(voiceselect);
+let rate_toggle = listen_btn.insertAdjacentHTML("afterend",
+`     <div class="uiunit">
+      <label for="rateFld">Speed: </label><input type="number" class="rateFld" min="0.5" max="2" step="0.1" value="1.2">
+      </div> `
+);
 
 
+window.onload = () =>{
+   listvoices();
+   all_voice();
+   rateAudio();
+}
+
+function listvoices(){
 playspeech.onvoiceschanged = () =>{
    
    voices = playspeech.getVoices();
    speech.voice = voices[0];
    voices.forEach((voice,i) =>(voiceselect.options[i]= new Option(voice.name,i)))
 }
+}
 
-
+function all_voice(){
 const voice = playspeech.getVoices();
 
 voices.forEach((voice, index) => {
@@ -32,14 +41,26 @@ voices.forEach((voice, index) => {
   option.textContent = voice.name;
   voiceselect.append(option);
 });
+}
 
 
+/*
 voiceselect.onchange = () =>{
     const selectedVoiceIndex = voiceselect.value;
     speech.voice = voices[selectedVoiceIndex];
+}*/
+
+function rateAudio(){ // probelmset
+   let rate_toggle = document.querySelector('.rateFld');
+   speech.rate = rate_toggle.getAttribute('value');
+   console.log(speech.rate);
 }
 
-function speak(){
+
+function speak(){ //single role function 
    speech.text = content_textbox.value;
    playspeech.speak(speech);
+   if(content_textbox.value ==  ""){
+      window.alert("not prompt, please enter prompt!!")
+   }
 };
